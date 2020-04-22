0f6f1be32cd6b2b36cf5ca6fb99ef8b346ed4625
Jimmy Blundell <jblundell123@gmail.com>
2020-04-22 18:06:15 -0500

    Added some comments for next time.

--- data-config.py
+++ data-config.py
@@ -22,3 +22,10 @@ counter = Counter(list_of_words)
 top_hundred = counter.most_common(100)
 print(top_hundred)
 
+# Now that I have a list of the top 100, I will go back through the csv,
+# splitting the lines into words (like above), and checking each word for presence in top 100.
+# I will create a new list out of said, and write that string to that particular row's index.
+# OR: I can create a whole new csv file! Adding each word that matches a top 100
+# to its particular location. This may be the better route. I also need to remove the last column (top 10).
+#
+
