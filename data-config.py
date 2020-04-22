ecfb8ac4005cfaa3f31f2b7c79597ad28be4a0e9
Jimmy Blundell <jblundell123@gmail.com>
2020-04-22 17:20:38 -0500

    Using collections module for counting feature for word frequencies.

--- data-config.py
+++ data-config.py
@@ -1,19 +1,24 @@
-import numpy as np
-import tensorflow as tf
-from tensorflow import keras
+#import numpy as np
+#import tensorflow as tf
+#from tensorflow import keras
 import csv as csv
+from collections import Counter
 
 # Replace brand names that appear only once with "Other"
 
 reader = csv.reader(open('ramen-ratings.csv', 'r'))
-outputfile = open('variety.txt', 'w')
 
 next(reader)
 
-#Print each individual word into a separate file.
+list_of_words = []
+
+#Grab each individual word and put into separate list
 for line in reader:
     for word in line[2].split():
-        outputfile.write(word + '\n')
-        print(word)
+        list_of_words.append(word)
 
+#Create list of top 100 used words
+counter = Counter(list_of_words)
+top_hundred = counter.most_common(100)
+print(top_hundred)
 
