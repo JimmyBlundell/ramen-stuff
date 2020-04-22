ff5a14f0bdbc95ae1c9bcb0cdc33151409720d4a
Jimmy Blundell <jblundell123@gmail.com>
2020-04-22 18:46:51 -0500

    Added more comments, algorithm for how I will process variety data.

--- data-config.py
+++ data-config.py
@@ -21,11 +21,27 @@ for line in reader:
 counter = Counter(list_of_words)
 top_hundred = counter.most_common(100)
 print(top_hundred)
+for item in top_hundred:
+    print(item)
 
-# Now that I have a list of the top 100, I will go back through the csv,
+
+'''Now that I have a list of the top 100, I will go back through the csv,
 # splitting the lines into words (like above), and checking each word for presence in top 100.
 # I will create a new list out of said, and write that string to that particular row's index.
 # OR: I can create a whole new csv file! Adding each word that matches a top 100
 # to its particular location. This may be the better route. I also need to remove the last column (top 10).
-#
+
+Upon a little thought, this will be it:
+Write to new csv:
+    Write csv[0]
+    Write csv[1]
+    For csv[2]:
+        create temp string
+        temp_list = csv[2].split
+        for item in list:
+            if in top 100, temp_string += item
+            (Figure out how to work with spaces here)
+        Write string to csv[2]
+    Continue for others, ignoring top 10.
+'''
 
