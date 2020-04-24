cb7144a36bb11788d5e23099c26cc45a8f37d6e6
Jimmy Blundell <jblundell123@gmail.com>
2020-04-24 17:48:49 -0500

    Finished pre-processing data (I think). Distilled brands and varieties down into new csv.

--- data-config.py
+++ data-config.py
@@ -7,8 +7,6 @@ from collections import Counter
 # Replace brand names that appear only once with "Other"
 
 reader = csv.reader(open('ramen-ratings.csv', 'r'))
-new_data = csv.writer(open('new-data.csv', 'w'))
-
 next(reader)
 
 #Grab each individual word and put into separate list
@@ -17,60 +15,54 @@ for line in reader:
     for word in line[2].split():
         list_of_words.append(word)
 
+
 #Create list of top 100 used words
 counter = Counter(list_of_words)
 counter = counter.most_common(100)
+
 
 #Add top 100 words to set for quicker access.
 top_hundred = set({})
 for word in counter:
     top_hundred.add(word[0])
 
-#Add brand names to string to keep track of # of occurrences
+
+#Add brand names to string to keep track of # of occurrences using count() method
 brand_names = ""
 reader = csv.reader(open('ramen-ratings.csv', 'r'))
 next(reader)
 
 for line in reader:
     brand_names += line[1] + '\n'
-print(brand_names)
-'Use brand_names.count(brand) to iterate through original list and alter if needed'
-
-
-
 
 
 #Edit csv, turning each line into list, then inserting back into csv
-'''for line in reader:
+reader = csv.reader(open('ramen-ratings.csv', 'r'))
+header = next(reader)
+
+writer = csv.writer(open('new-data.csv', 'w'))
+writer.writerow(header)
+
+
+#For reference: Review #,Brand,Variety,Style,Country,Stars,Top Ten
+for line in reader:
     lst = list(line)
-    print(lst)
-'''
+    #All brands appearing once should be "Other"
+    if (brand_names.count(lst[1]) == 1):
+        lst[1] = "Other"
 
+    #Include only variety words in top 100
+    temp = ""
+    words = line[2].split()
+    for i in range(len(words)):
+        if (words[i] in top_hundred):
+            if (len(temp.split()) == 0):
+                temp += words[i]
+            else:
+                temp += ' ' + words[i]
 
-
-#
-
-
-'''Now that I have a list of the top 100, I will go back through the csv,
-# splitting the lines into words (like above), and checking each word for presence in top 100.
-# I will create a new list out of said, and write that string to that particular row's index.
-# OR: I can create a whole new csv file! Adding each word that matches a top 100
-# to its particular location. This may be the better route. I also need to remove the last column (top 10).
-
-Upon a little thought, this will be it:
-Write to new csv:
-    Write csv[0]
-    Write csv[1]
-    For csv[2]:
-        create temp string
-        temp_list = csv[2].split
-        for item in list:
-            if in top 100, temp_string += item
-            (Figure out how to work with spaces here)
-        Write string to csv[2]
-    Continue for others, ignoring top 10.
-    
-As for brand names: Use map with key as brand, value as count. Search through, and increment each time if not found.
-Then, go back through csv. Search all brand names again. If count == 1, change to other. Else, continue.
-'''
+    lst[2] = temp
+    #Ignore Top Ten
+    lst[6] = ""
+    writer.writerow(lst)
 
