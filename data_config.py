82d000271b5abee6d87f75d0cfe32c1efa789afc
Jimmy Blundell <jblundell123@gmail.com>
2020-05-01 18:21:57 -0500

    Fixed indentation.

--- data_config.py
+++ data_config.py
@@ -2,126 +2,126 @@ import csv as csv
 import numpy as np
 from collections import Counter
 
-#def configure_csv(oldFile, training_data, test_data, validation_data):
+def configure_csv(oldFile, training_data, test_data, validation_data):
 
-oldFile = "ramen-ratings.csv"
-training_data = "training-data.csv"
-test_data = "test-data.csv"
-validation_data = "validation-data.csv"
+    oldFile = "ramen-ratings.csv"
+    training_data = "training-data.csv"
+    test_data = "test-data.csv"
+    validation_data = "validation-data.csv"
 
-reader = csv.reader(open(oldFile, 'r'))
-data_len = len(list(reader))
-reader = csv.reader(open(oldFile, 'r')) #Initializing again because I'm not sure how else to do it in the moment :)
-next(reader)
+    reader = csv.reader(open(oldFile, 'r'))
+    data_len = len(list(reader))
+    reader = csv.reader(open(oldFile, 'r')) #Initializing again because I'm not sure how else to do it in the moment :)
+    next(reader)
 
-#Create lists of words in varieties, styles, brands, and country
-list_of_varieties = []
-list_of_brands = []
-list_of_styles = []
-list_of_countries = []
-for line in reader:
-    for word in line[2].split():
-        list_of_varieties.append(word)
-    list_of_brands.append(line[1])
-    list_of_styles.append(line[3])
-    list_of_countries.append(line[4])
+    #Create lists of words in varieties, styles, brands, and country
+    list_of_varieties = []
+    list_of_brands = []
+    list_of_styles = []
+    list_of_countries = []
+    for line in reader:
+        for word in line[2].split():
+            list_of_varieties.append(word)
+        list_of_brands.append(line[1])
+        list_of_styles.append(line[3])
+        list_of_countries.append(line[4])
 
-#Remove duplicates
-list_of_brands = list(dict.fromkeys(list_of_brands))
-list_of_styles = list(dict.fromkeys(list_of_styles))
-list_of_countries = list(dict.fromkeys(list_of_countries))
-
-
-#Create list of top 100 used words
-counter_words = Counter(list_of_varieties)
-counter_words = counter_words.most_common(100)
-list_of_varieties = []
-for word in counter_words:
-    list_of_varieties.append(word[0])
+    #Remove duplicates
+    list_of_brands = list(dict.fromkeys(list_of_brands))
+    list_of_styles = list(dict.fromkeys(list_of_styles))
+    list_of_countries = list(dict.fromkeys(list_of_countries))
 
 
-#Create dictionary mapping index to each word in top 100
-varietyDict = {}
-for i in range(0, 100):
-    varietyDict[list_of_varieties[i]] = i
+    #Create list of top 100 used words
+    counter_words = Counter(list_of_varieties)
+    counter_words = counter_words.most_common(100)
+    list_of_varieties = []
+    for word in counter_words:
+        list_of_varieties.append(word[0])
 
 
-#For later: map each element in lists to unique integer to translate everything to numbers - may need this for personal use when NN spits out results? Idk man
-brandDict = {}
-for i in range(len(list_of_brands)):
-    brandDict[list_of_brands[i]] = i+1
-brandDict["Other"] = len(brandDict) + 1
-
-styleDict = {}
-for i in range(len(list_of_styles)):
-    styleDict[list_of_styles[i]] = i+1
-
-countryDict = {}
-for i in range(len(list_of_countries)):
-    countryDict[list_of_countries[i]] = i+1
+    #Create dictionary mapping index to each word in top 100
+    varietyDict = {}
+    for i in range(0, 100):
+        varietyDict[list_of_varieties[i]] = i
 
 
-#Add brand names to string to keep track of # of occurrences using count() method
-brand_names = ""
-reader = csv.reader(open(oldFile, 'r'))
-next(reader)
+    #For later: map each element in lists to unique integer to translate everything to numbers - may need this for personal use when NN spits out results? Idk man
+    brandDict = {}
+    for i in range(len(list_of_brands)):
+        brandDict[list_of_brands[i]] = i+1
+    brandDict["Other"] = len(brandDict) + 1
 
-for line in reader:
-    brand_names += line[1] + '\n'
+    styleDict = {}
+    for i in range(len(list_of_styles)):
+        styleDict[list_of_styles[i]] = i+1
 
-
-#Edit csv, turning each line into list, then inserting back into csv
-reader = csv.reader(open(oldFile, 'r'))
-header = next(reader)
-
-header[2] = "Variety1"
-for i in range(99):
-    header.insert(i+3, "Variety" + str(i+2))
-
-print(header)
-
-writer1 = csv.writer(open(training_data, 'w'))
-writer2 = csv.writer(open(test_data, 'w'))
-writer3 = csv.writer(open(validation_data, 'w'))
-#TODO: Not sure yet if I'll need these - I may at the very least need the header for features/label?
-#writer1.writerow(header[:-1])
-#writer2.writerow(header[:-1])
-#writer3.writerow(header[:-1])
-
-count = 0
-#Make new CSV file
-for line in reader:
-    count += 1
-    lst = list(line)
-
-    #Convert ratings to floats, ignoring the few reviews that are unrated
-    if (lst[5] == "Unrated"):
-        continue
-
-    #All brands appearing once should be "Other"
-    if (brand_names.count(lst[1]) == 1):
-        lst[1] = brandDict["Other"]
-    else:
-        lst[1] = brandDict[lst[1]]
+    countryDict = {}
+    for i in range(len(list_of_countries)):
+        countryDict[list_of_countries[i]] = i+1
 
 
-    style = lst[3]
-    country = lst[4]
-    stars = float(lst[5])
+    #Add brand names to string to keep track of # of occurrences using count() method
+    brand_names = ""
+    reader = csv.reader(open(oldFile, 'r'))
+    next(reader)
 
-    #Add all varietes as a frequency according to an index in an array (this is extending the columns of the csv file)
-    array = list([0]*100)
-    for word in line[2].split():
-        if word in varietyDict:
-            array[varietyDict[word]] += 1
-    lst[2:101] = array
+    for line in reader:
+        brand_names += line[1] + '\n'
 
-    lst.extend([styleDict[style], countryDict[country], stars])
 
-    #Create training, test, and validation sets, ignoring the Review # and "Top 10"
-    if (count <= data_len*0.8):
-        writer1.writerow(lst[1:105])
-    elif (count <= data_len*0.9):
-        writer2.writerow(lst[1:105])
-    else:
-        writer3.writerow(lst[1:105])
+    #Edit csv, turning each line into list, then inserting back into csv
+    reader = csv.reader(open(oldFile, 'r'))
+    header = next(reader)
+
+    header[2] = "Variety1"
+    for i in range(99):
+        header.insert(i+3, "Variety" + str(i+2))
+
+    print(header)
+
+    writer1 = csv.writer(open(training_data, 'w'))
+    writer2 = csv.writer(open(test_data, 'w'))
+    writer3 = csv.writer(open(validation_data, 'w'))
+    #TODO: Not sure yet if I'll need these - I may at the very least need the header for features/label?
+    #writer1.writerow(header[:-1])
+    #writer2.writerow(header[:-1])
+    #writer3.writerow(header[:-1])
+
+    count = 0
+    #Make new CSV file
+    for line in reader:
+        count += 1
+        lst = list(line)
+
+        #Convert ratings to floats, ignoring the few reviews that are unrated
+        if (lst[5] == "Unrated"):
+            continue
+
+        #All brands appearing once should be "Other"
+        if (brand_names.count(lst[1]) == 1):
+            lst[1] = brandDict["Other"]
+        else:
+            lst[1] = brandDict[lst[1]]
+
+
+        style = lst[3]
+        country = lst[4]
+        stars = float(lst[5])
+
+        #Add all varietes as a frequency according to an index in an array (this is extending the columns of the csv file)
+        array = list([0]*100)
+        for word in line[2].split():
+            if word in varietyDict:
+                array[varietyDict[word]] += 1
+        lst[2:101] = array
+
+        lst.extend([styleDict[style], countryDict[country], stars])
+
+        #Create training, test, and validation sets, ignoring the Review # and "Top 10"
+        if (count <= data_len*0.8):
+            writer1.writerow(lst[1:105])
+        elif (count <= data_len*0.9):
+            writer2.writerow(lst[1:105])
+        else:
+            writer3.writerow(lst[1:105])
